de7ffb546b01e81415de42681d99f353d9447ce4
JUC <jason1202u@gmail.com>
2022-11-10 13:05:10 -0600

    falta correcion de la funcion mayorMenor, y hacer funcionar la solicitud de datos.

---
+++ main.js
@@ -0,0 +1,72 @@
+/**
+Ejercicio 1:
+Escriba un programa que solicite al usuario una lista de números y que, a partir de ellos,
+calcule e imprima en la consola los siguientes valores estadísticos:
+- Promedio
+- Mediana
+- Valor mínimo
+- Valor máximo
+El programa debe solicitar números al usuario y almacenarlos en un array. El programa
+continuará solicitando números hasta que el usuario ingrese un string vacío. Cuando esto
+ocurra se calcularán e imprimirán los resultados.
+Si el usuario ingresa algún valor que no sea un número, este será ignorado y no se
+agregará al array.
+
+ */
+
+let valores = [5,1,65,115,75];
+let min = [0];
+let max = [0];
+let flag = true
+let inc = 0
+
+while(flag){
+  let numeros = prompt("Digite un valor, cuando no desee ingresar mas numeros precione cuanquier otra letra: ");
+  if(numeros === Number){
+    valores.push(numeros);
+  }else if (numeros === ''){
+    break;
+  }
+  inc++
+  console.log(valores);
+}
+
+
+function mayorMenor (valores){
+  for (let i = 0; i < valores.length; i++){
+    if (min > valores[i]){
+      min = valores[i];
+    };
+  
+    if (max < valores[i]){
+      max = valores[i];
+    };
+  };
+
+  console.log("El numero menor ingresado fue: " + min);
+  console.log("El numero mayor ingresado fue: " + max);
+};
+
+function promedio (valores){
+  let sum = valores.reduce((before, next) => next += before);
+  let prom = sum / valores.length;
+  return ("El promedio de los valores ingresados es: " + prom)
+};
+
+function mediana(valores) {
+  valores.sort((a, b) => a - b);
+  let der = Math.floor((valores.length - 1) / 2);
+  let izq = Math.ceil((valores.length - 1) / 2);
+  let mediana = (valores[der] + valores[izq]) / 2;
+  console.log("La media del conjunto devalores ingresados es: " + mediana)
+}
+
+
+function main(){
+  mayorMenor(valores);
+  console.log(promedio(valores));
+  mediana(valores);
+};
+
+main();
+
